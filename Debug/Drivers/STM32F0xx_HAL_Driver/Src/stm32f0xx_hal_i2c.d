9ebc3b3ceed162e61b6862a849f23ad95bdffcc5
David Molony <david.molony@cambtek.com>
2019-05-02 16:50:49 +0100

    1st commit

---
+++ Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.d
@@ -0,0 +1,77 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Inc/stm32f0xx_hal_conf.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Inc/stm32f0xx_hal_conf.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f042x6.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/core_cm0.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+C:/Users/Lenovo/Documents/F042\ USARTtest/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
